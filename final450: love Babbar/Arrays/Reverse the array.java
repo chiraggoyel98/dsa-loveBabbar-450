e605f4e68871b4ef37bf09f7d18f89821716e48e
chiraggoyel98 <56262887+chiraggoyel98@users.noreply.github.com>
2022-01-23 10:02:15 +0530

    Create Reverse the array.java

---
+++ final450: love Babbar/Arrays/Reverse the array.java
@@ -0,0 +1,8 @@
+//Q. Write a program to reverse an array or string
+/*
+four ways:
+1) Using two pointers , one as start=0 and other end=n-1
+ 
+    Time complexity: O(n)
+    no additional space required
+    */
