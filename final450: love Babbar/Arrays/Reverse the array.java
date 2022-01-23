28fa0c31cd49750614caa9669aebde6e1eb3fcef
chiraggoyel98 <56262887+chiraggoyel98@users.noreply.github.com>
2022-01-23 10:14:59 +0530

    Update Reverse the array.java

--- final450: love Babbar/Arrays/Reverse the array.java
+++ final450: love Babbar/Arrays/Reverse the array.java
@@ -1,8 +1,35 @@
 //Q. Write a program to reverse an array or string
 /*
-four ways:
+
 1) Using two pointers , one as start=0 and other end=n-1
  
     Time complexity: O(n)
     no additional space required
     */
+public static void reverseArray(int[] arr) {
+   int start = 0;
+   int end = arr.length-1;
+   while(start<end) {
+    int tmp = arr[start];
+    arr[start] = arr[end];
+    arr[end] = tmp;
+    
+    start++;
+    end--;
+   }
+}
+
+/*
+2). Recursive way:
+ Change the definition of func in recursive form:
+ 
+ 
+ public static void reverseArray(int[] arr, int start, int end) {
+  if(start>=end) return;
+  swap(arr[start],arr[end]);
+  reverseArray(int[] arr, start+1, end-1);
+  }
+  */
+  
+  
+  
